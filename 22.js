1e9909648b4f07f1fec1014637211e5a8b378050
Weili.Cai <caiwlwinnie@gmail.com>
2017-04-13 10:50:38 +0800

    第二十二章 高级技巧

--- 22.js
+++ 22.js
@@ -177,3 +177,113 @@ function throttle(method, context) {
 }
 
 //自定义事件
+function EventTarget() {
+    this.handlers = {};
+}
+
+EventTarget.prototype = {
+    constructor: EventTarget,
+    addHandler: function (type, handler) {
+        if (typeof this.handlers[type] == "undefined") {
+            this.handlers[type] = [];
+        }
+
+        this.handlers[type].push(handler);
+    },
+    fire: function (event) {
+        if (!event.target) {
+            event.target = this;
+        }
+        if (this.handlers[event.type] instanceof Array) {
+            var handlers = this.handlers[event.type];
+            for (var i = 0, len = handlers.length; i < len; i++) {
+                handlers[i](event);
+            }
+        }
+    },
+    removeHandler: function (type, handler) {
+        if (typeof this.handlers[type] != "undefined") {
+            var handlers = this.handlers[type];
+            for (var i = 0, len = handlers.length; i < len; i++) {
+                if (handlers[i] == handler) {
+                    break;
+                }
+            }
+            handlers.splice(i, 1); //移除
+        }
+    }
+};
+
+var DragDrop = function () {
+
+    var dragdrop = new EventTarget(),
+        dragging = null,
+        diffX = 0,
+        diffY = 0;
+
+    function handleEvent(event) {
+        //获取事件和目标
+        event = EventUtil.getEvent(event);
+        var target = EventUtil.getTarget(event);
+
+        //确定事件类型
+        switch (event.type) {
+            case "mousedown":
+                if (target.className.indexOf("draggable") > -1) {
+                    dragging = target;
+                    diffX = event.clientX - target.offsetLeft;
+                    diffY = event.clientY - target.offsetTop;
+                    //触发自定义事件
+                    dragdrop.fire({type: "dragstart", target: dragging, x: event.clientX, y: event.clientY});
+                }
+                break;
+            case "mousemove":
+                if (dragging != null) {
+
+                    //指定位置
+                    dragging.style.left = (event.clientX - diffX) + "px";
+                    dragging.style.top = (event.clientY - diffY) + "px";
+                    //触发自定义事件
+                    dragdrop.fire({type: "drag", target: dragging, x: event.clientX, y: event.clientY});
+                }
+                break;
+            case "mouseup":
+
+                //触发自定义事件
+                dragdrop.fire({type: "dragend", target: dragging, x: event.clientX, y: event.clientY});
+
+                dragging = null;
+                break;
+        }
+    }
+
+    dragdrop.enable = function () {
+        EventUtil.addHandler(document, "mousedown", handleEvent);
+        EventUtil.addHandler(document, "mousemove", handleEvent);
+        EventUtil.addHandler(document, "mouseup", handleEvent);
+    };
+    dragdrop.disable = function () {
+        EventUtil.removeHandler(document, "mousedown", handleEvent);
+        EventUtil.removeHandler(document, "mousemove", handleEvent);
+        EventUtil.removeHandler(document, "mouseup", handleEvent);
+    };
+
+    return dragdrop;
+}();
+
+//TEST
+DragDrop.addHandler("dragstart", function (event) {
+    var status = document.getElementById("status");
+    status.innerHTML = "Started dragging " + event.target.id;
+});
+DragDrop.addHandler("drag", function (event) {
+    var status = document.getElementById("status");
+    status.innerHTML += "<br/> Dragged " + event.target.id + " to (" + event.x + "," + event.y + ")";
+});
+DragDrop.addHandler("dragend", function (event) {
+    var status = document.getElementById("status");
+    status.innerHTML = "<br/> Dropped " + event.target.id + " to (" + event.x + "," + event.y + ")";
+});
+
+DragDrop.enable();
+
